17a6121f45561e471b03c4e20d5133cdee1c65ae
YouJeen-ru <1.rouch@mail.ru>
2021-03-03 20:09:36 +0300

    Section search completed

--- js/main.js
+++ js/main.js
@@ -5,4 +5,14 @@ $(function(){
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnNext"><img src="../images/arrow-right.svg" alt="arrow-right"></button>',
     })
 
+    $('.search__tabs-item').on('click', function (e){
+        e.preventDefault()
+
+        $('.search__tabs-item').removeClass('search__tabs-item--active')
+        $('.search__content-item').removeClass('search__content-item--active')
+
+        $(this).addClass('search__tabs-item--active')
+        $($(this).attr('href')).addClass('search__content-item--active')
+    })
+
 });
