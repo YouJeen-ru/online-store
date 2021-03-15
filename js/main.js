251f9d2817c0b3391ae9305fd19905f20ce0f03c
YouJeen-ru <1.rouch@mail.ru>
2021-03-15 16:37:23 +0300

    Section products completed add new slider for section

--- js/main.js
+++ js/main.js
@@ -15,4 +15,15 @@ $(function(){
         $($(this).attr('href')).addClass('tabs-content--active')
     })
 
+    $('.product-item__favorite').on('click', function () {
+        $(this).toggleClass('product-item__favorite--active')
+    })
+
+    $('.product-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnPrev"><img src="../images/arrow-black-left.svg" alt="arrow-left"></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnNext"><img src="../images/arrow-black-right.svg" alt="arrow-right"></button>',
+    })
+
 });
