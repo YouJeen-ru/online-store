4cd413d9580746c14c44bdafab49057fbf128567
YouJeen-ru <1.rouch@mail.ru>
2021-04-04 23:38:53 +0300

    Adaptive layout banner-section completed

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ $(function(){
         dots: true,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnPrev"><img src="../images/arrow-left.svg" alt="arrow-left"></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnNext"><img src="../images/arrow-right.svg" alt="arrow-right"></button>',
+        responsive: [
+            {
+                breakpoint: 969,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     })
 
     $('.tab').on('click', function (e){
