06f1dce6570c41b07cc4308f6df1fbfec3015729
YouJeen-ru <1.rouch@mail.ru>
2021-04-08 16:25:09 +0300

    Adaptive layout products section completed

--- js/main.js
+++ js/main.js
@@ -32,6 +32,36 @@ $(function(){
         slidesToScroll: 1,
         prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnPrev"><img src="../images/arrow-black-left.svg" alt="arrow-left"></button>',
         nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnNext"><img src="../images/arrow-black-right.svg" alt="arrow-right"></button>',
+        responsive: [
+            {
+                breakpoint: 1301,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 3,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 870,
+                settings: {
+                    slidesToShow: 2,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 590,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            }
+        ]
     })
 
     $('.filter-style').styler();
