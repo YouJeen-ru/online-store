8300d4fc780961a6d487fca3b7d8883f3202aca4
YouJeen-ru <1.rouch@mail.ru>
2021-04-11 16:47:35 +0300

    Adaptive layout catalog page completed

--- js/main.js
+++ js/main.js
@@ -102,4 +102,8 @@ $(function(){
         $(this).toggleClass('footer__topdrop--active')
     })
 
+    $('.aside__btn').on('click', function () {
+        $(this).next().slideToggle()
+    })
+
 });
